a9a4709488d358dac358e5c0394bc1e8185ac608
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-01-06 13:29:10 +0300

    registerPage,login работает и редиректит

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,23 +2,32 @@ import Form from "./Form"
 import { useDispatch } from "react-redux"
 import { setUser } from '../store/slices/UserSlice'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
     const dispatch = useDispatch()
+    const push = useNavigate()
 
 
     const handleLogin = (email, password) => {
         const auth = getAuth();
         signInWithEmailAndPassword(auth, email, password)
-            .then(console.log)
-            .catch(console.error)
+            .then(({ user }) => {
+                dispatch(setUser({
+                    email: user.email,
+                    id: user.uid,
+                    token: user.accessToken,
+                }))
+                push('/');
+            })
+            .catch(()=>alert('Invaled user!'))
     }
     return (
         <div className="">
-            <Form 
-            title='login'
-            handleClick={handleLogin}
+            <Form
+                title='login'
+                handleClick={handleLogin}
             />
         </div>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
 import { Navigate } from "react-router-dom"
+import { useAuth } from '../hooks/use-auth'
+import { useDispatch } from "react-redux";
+import { removeUser } from '../store/slices/UserSlice'
 
 const HomePage = () => {
-    return (
-      <Navigate to='/login'/>
-    )
+  const dispatch = useDispatch()
+
+  const { isAuth, email } = useAuth();
+
+  return isAuth ? (
+    <div >
+
+      <h1>Welcome</h1>
+
+      <button
+        onClick={() => dispatch(removeUser())}>
+        Log out from:{email}
+      </button>
+
+    </div>
+  ) : (
+    <Navigate to='/login' />
+  )
 }
 
 export default HomePage
